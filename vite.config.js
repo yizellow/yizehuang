9d810de710cc825a4ae33313c0e9717fa1998177
Yi-Ze Huang <b10821056@gmcc.tnua.edu.tw>
2024-11-07 18:38:44 +0800

    test3

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/yizehuang/',
-  plugins: [
-    vue(),
-  ],
+  // base:'/yizehuang/',
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
